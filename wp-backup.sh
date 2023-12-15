3f86a18d8aea89a194fad4b2d82d227f5f21c70d
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-12-15 16:44:42 +0500

    update WordPress backup script

--- wp-backup.sh
+++ wp-backup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 WORDPRESS_DIR="/var/www/html/2112121/wordpress"
-
 BACKUP_DIR="/home/mustan/"
+REMOTE_USER="remote_username"
+REMOTE_HOST="remote_host_address"
+REMOTE_BACKUP_DIR="remote_backup_directory"
 
 TIMESTAMP=$(date +"%d%m_%H%M")
 
-tar -czf $BACKUP_DIR/wp-backup_$TIMESTAMP.tar.gz -C $WORDPRESS_DIR .
+tar -czf "$BACKUP_DIR/backup_$TIMESTAMP.tar.gz" -C "$WORDPRESS_DIR" .
 
-echo "Backup Completed successfully!"
+scp "$BACKUP_DIR/backup_$TIMESTAMP.tar.gz" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BACKUP_DIR"
+
+if [ $? -eq 0 ]; then
+    echo "Backup transferred successfully!"
+else
+    echo "Backup transfer failed."
+fi
+
+rm -f "$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
