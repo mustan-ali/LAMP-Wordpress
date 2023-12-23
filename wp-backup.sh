6c34edbb57bc86401ce6a3fb52336d8490179035
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-12-23 18:11:12 +0500

    update backup script

--- wp-backup.sh
+++ wp-backup.sh
@@ -4,4 +4,4 @@ REG_NO=2112121
 REMOTE_USER="remote_username"
 REMOTE_HOST="remote_host_address"
 
-rsync -r $REMOTE_USER@$REMOTE_HOST:/var/www/html/$REG_NO ~/$REG_NO/
+rsync -r $REMOTE_USER@$REMOTE_HOST:/var/www/html/$REG_NO ~/
